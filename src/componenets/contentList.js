c9eda064bbc87bdb5ef3f55c94a4be519225e61a
Tony-Tyrant <tony.tyrant@gmail.com>
2023-06-15 15:04:38 +0800

    minor bugs fixed & added home button

--- src/componenets/contentList.js
+++ src/componenets/contentList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const contents = {
+  intro: 'HOME',
   aboutMe: 'ABOUT ME',
   education: 'EDUCATION & CERTIFICATES',
   workExp: 'WORK EXPERIENCE',
