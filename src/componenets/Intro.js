3cae89661a8fbac25ef45dc5353ee664a193cabe
Tony-Tyrant <tony.tyrant@gmail.com>
2023-06-19 14:49:00 +0800

    Edited the introduction

--- src/componenets/Intro.js
+++ src/componenets/Intro.js
@@ -4,10 +4,10 @@ export default function Intro() {
     return (
         <div className="intro">
             <p>
-                This web-page CV is made by a former engineer
+                This web-page is made by a former BS engineer
                 who worked in the building industry and
-                seeking for an opportunity for career transition
-                through self-taught programming.
+                is seeking for an opportunity for a career transition
+                into the IT industry through self-taught programming.
                 This site shows the skills of using HTML,
                 CSS, JavaScript, React, Git .etc.
             </p>
