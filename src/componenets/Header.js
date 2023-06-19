15ccfd58ca2b56eb2bd0cb74d34d5ea5c790d7fc
Tony-Tyrant <tony.tyrant@gmail.com>
2023-06-19 14:44:21 +0800

    Edited the heading

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="app-header">
-            <h1>You're Looking at Tony Ho's CV</h1>
+            <h1>You're Looking at Tony Ho's Portfolio</h1>
 
         </header>
     )
