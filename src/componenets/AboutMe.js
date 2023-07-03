04b3adb8e0af66696aee761819cb0250e1e9a2d1
Tony-Tyrant <tony.tyrant@gmail.com>
2023-07-03 21:55:36 +0800

    updated About Me

--- src/componenets/AboutMe.js
+++ src/componenets/AboutMe.js
@@ -5,6 +5,11 @@ export default function AboutMe() {
         <section id="aboutme">
             <div className='mt-5 mx-md-5'>
                 <p className='lead'>
+                    I am a self-taught programmer. I have honed my skills in programming languages such as
+                    <b> HTML</b>,<b> CSS</b> and <b>JavaScript</b> and have experience in front-end frameworks
+                    such as <b>React.js</b>, <b>React-Redux</b>, <b>Bootstrap</b> and <b>Git</b> through
+                    taking online courses and completing personal projects.<br />
+                    <br />
                     I graduated with a Bachelor of Building Services Engineering.
                     During almost 10 years of life being a BS engineer,
                     I gained valuable experience in <b>project management</b>,
@@ -12,13 +17,7 @@ export default function AboutMe() {
                     Due to a conversation with a friend of mine,
                     I started to learn programming through various online courses
                     and personal projects after work.
-                    Meanwhile, I discovered my passion for it and decided to make a career change.<br />
-                    <br />
-                    As a self-taught programmer, I have honed my skills in programming languages such as
-                    <b> HTML</b>,<b> CSS</b> and <b>JavaScript</b> and have experience working with front-end frameworks
-                    such as <b>React.js</b> and <b>Bootstrap</b>. I have also taken online courses and completed
-                    personal projects to expand my knowledge and stay up-to-date with the latest
-                    industry trends.<br />
+                    Meanwhile, I discovered my passion for it and decided to make a career change.<br />                    
                     <br />
                     My career transition has taught me the importance of adaptability, continuous learning,
                     and perseverance. I am excited about the opportunities and challenges that the IT industry
