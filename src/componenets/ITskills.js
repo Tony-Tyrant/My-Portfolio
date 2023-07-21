a3050fcb146f838906224d7fc064c2762bfdfafd
Tony-Tyrant <tony.tyrant@gmail.com>
2023-07-21 14:48:22 +0800

    Added PharmacistConnect

--- src/componenets/ITskills.js
+++ src/componenets/ITskills.js
@@ -15,7 +15,7 @@ export default function ITSkills() {
             </div>
 
             <h2 className="mt-5 pt-5 mb-3 text-uppercase text-decoration-underline fw-bold">personal project</h2>
-            <div className="lead pb-4" id="appointmentmaker">
+            <div className="lead pb-4">
                 <h4>
                     <a
                         href="https://4-6-coffee-timer.netlify.app/"
@@ -37,6 +37,29 @@ export default function ITSkills() {
                         <li>Appling Bootstrap to achieve responsive design.</li>
                         <li>Transmiting variables to multi-component by React Context hooks.</li>
                         <li>Controlling the timer with the help of React useEffect and useState hooks.</li>
+                    </ul>
+                </p>
+            </div>
+            <div className="lead pb-4">
+                <h4>
+                    <a
+                        href="https://pharmacistsconnect.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-decoration-none text-body-secondary"
+                        title="4:6 Coffee Brewing System Timer">
+                        Pharmacists Connect&nbsp;
+                    </a>
+                    <img 
+                            src="resources/link_symbol.png" 
+                            alt="link"
+                            style={{height: "0.7em"}}/>
+                </h4>
+                <p>
+                    <ul>
+                        <li>A web showing where to look for a registered pharmacist.</li>
+                        <li>Fetching pharmacy data from Strapi Headless CMS.</li>
+                        <li>Locating the connected pharmacist with Google Map React API.</li>
                     </ul>
                 </p>
             </div>
